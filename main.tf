30594acc3b12133d925b7bb65f9dcd063adbbc4d
terraAI <terraAI@example.com>
2024-07-10 09:51:05 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -13,21 +13,10 @@ provider "google" {
   project     = "abc"
 }
 
-resource "google_app_engine_application" "default" {
-  location_id = "us-central1"
-  name        = "appengine-app"
-}
-
-resource "google_app_engine_service" "default" {
-  name     = "default"
-  location = "us-central1"
-  application = google_app_engine_application.default.name
-}
-
 resource "google_container_cluster" "default" {
-  name     = "gke-cluster"
-  location = "us-central1-a"
-  initial_node_count = 1
+  name     = "gcp-kubernetes-cluster"
+  location = "us-central1"
+  initial_node_count = 3
   node_config {
     machine_type = "e2-medium"
   }
@@ -35,11 +24,34 @@ resource "google_container_cluster" "default" {
     username = "admin"
   }
   network = "default"
+  subnetwork = "projects/gcp-project-id/regions/us-central1/subnetworks/default"
 }
 
-resource "google_dns_managed_zone" "default" {
-  name     = "my-zone"
-  dns_name = "example.com."
-  description = "My DNS zone"
-  project = "abc"
+resource "google_cloudfunctions_function" "default" {
+  name     = "gcp-cloud-function"
+  runtime  = "nodejs16"
+  entry_point = "helloHTTP"
+  source_archive_bucket = "gcp-cloud-function-bucket"
+  source_archive_object = "gcp-cloud-function.zip"
+  trigger_http = true
+  region = "us-central1"
+}
+
+resource "google_app_engine_application" "default" {
+  location_id = "us-central1"
+  name        = "gcp-app-engine-app"
+}
+
+resource "google_app_engine_service" "default" {
+  name     = "gcp-app-engine-service"
+  location = "us-central1"
+  application = google_app_engine_application.default.name
+  runtime = "nodejs16"
+  entry_point = "app.js"
+  env = "standard"
+  scaling {
+    manual_scaling {
+      instances = 1
+    }
+  }
 }
