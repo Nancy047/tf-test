5354808d40ce319513c64afc8821730975ff32b1
terraAI <terraAI@example.com>
2024-07-10 09:28:59 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -13,20 +13,9 @@ provider "google" {
   project     = "abc"
 }
 
-resource "google_app_engine_application" "default" {
-  location_id = "us-central1"
-  name        = "appengine-app"
-}
-
-resource "google_app_engine_service" "default" {
-  name     = "default"
-  location = "us-central1"
-  application = google_app_engine_application.default.name
-}
-
 resource "google_container_cluster" "default" {
-  name     = "gke-cluster"
-  location = "us-central1-a"
+  name     = "gcp-kubernetes-engine-cluster"
+  location = "us-central1"
   initial_node_count = 1
   node_config {
     machine_type = "e2-medium"
@@ -35,11 +24,27 @@ resource "google_container_cluster" "default" {
     username = "admin"
   }
   network = "default"
+  subnetwork = "projects/gcp-project-id/regions/us-central1/subnetworks/default"
 }
 
-resource "google_dns_managed_zone" "default" {
-  name     = "my-zone"
-  dns_name = "example.com."
-  description = "My DNS zone"
-  project = "abc"
+resource "google_bigquery_data_exchange" "default" {
+  dataset_id = "analytics_hub_dataset"
+  display_name = "Analytics Hub Dataset"
+  data_exchange_id = "analytics_hub_exchange"
+  description = "Analytics Hub Data Exchange"
+  primary_exchange_asset {
+    asset_type = "dataset"
+    dataset {
+      dataset_id = "analytics_hub_dataset"
+    }
+  }
+  location = "us"
+}
+
+resource "google_iap_web_type_app_engine_version" "default" {
+  app_id = "gcp-iap-app-engine-app"
+  version_id = "gcp-iap-app-engine-version"
+  oauth2_client_id = "gcp-iap-client-id"
+  oauth2_client_secret = "gcp-iap-client-secret"
+  enabled = true
 }
